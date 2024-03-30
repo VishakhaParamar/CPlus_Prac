65de3ce14fa2202479b574e6b7ebd8ad9ceabc46
Meet <149648810+Meet-official@users.noreply.github.com>
2024-03-30 20:23:48 +0530

    Update CGPA.cpp

--- CGPA.cpp
+++ CGPA.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int nsub;
-    double totalcredits = 0.0;
-    double totalgrades = 0.0;
+    int n_sub;
+    float totalcredits = 0.0;
+    float totalgrades = 0.0;
     cout << "Enter the total number of subjects: \n";
-    cin >> nsub;
-    for (int i = 1; i <= nsub; ++i) {
-        double credit, grade;
-        cout << "Enter credit for "<<i<<"subject "<<i<<": \n";
+    cin >> n_sub;
+    for (int i = 1; i <= n_sub; i++) {
+        float credit, grade;
+        cout << "Enter credit for subject "<<i<<": \n";
         cin >> credit;
-        cout << "Enter grade for "<<i<<"subject "<<i<<": \n";
+        cout << "Enter grade for subject "<<i<<": \n";
         cin >> grade;
         totalcredits += credit;
         totalgrades += credit * grade;}
     if(totalcredits>0){
-    double cgpa = totalgrades / totalcredits;
+    float cgpa = totalgrades / totalcredits;
     cout << "Your CGPA is: \n" << cgpa;}
     else{
     cout<<"Credit cant be 0";}
