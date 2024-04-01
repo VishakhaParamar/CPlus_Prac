6345953455533a7066c8a6f6f804ef297ed915dc
Meet <149648810+Meet-official@users.noreply.github.com>
2024-04-01 18:42:33 +0530

    Update Stack & Queue.cpp

--- Stack & Queue.cpp
+++ Stack & Queue.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 void stackDemo() {
     stack<int> myStack;
-    //Push ele. into the stack
+    // Push ele. into the stack
     myStack.push(10);
     myStack.push(20);
     myStack.push(30);
-    //Print ele. in the stack
+    // Print ele. in the stack
     cout << "Stack elements: ";
     while (!myStack.empty()) {
-        //it will work when it's not null
+        // It will work when it's not null
         cout << myStack.top() << " ";
         myStack.pop();
     }
@@ -19,24 +19,24 @@ void stackDemo() {
 }
 void queueDemo() {
     queue<int> myQueue;
-    //Push ele. into the queue
+    // Enqueue(Push) ele. into the queue
     myQueue.push(10);
     myQueue.push(20);
     myQueue.push(30);
-    //Print ele. in the queue
+    // Print ele. in the queue
     cout << "Queue elements: ";
     while (!myQueue.empty()) {
-        //it will work when it's not null
+        // It will work when it's not null
         cout << myQueue.front() << " ";
         myQueue.pop();
     }
     cout << endl;
 }
 int main() {
-    // output comes in LIFO order
+    // Output comes in LIFO order
     cout << "Stack Example: " << endl;
     stackDemo();
-    // output comes in FIFO order
+    // Output comes in FIFO order
     cout << "Queue Example: " << endl;
     queueDemo();
     return 0;
