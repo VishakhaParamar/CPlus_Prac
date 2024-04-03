863024e88f872ea61f32543446111e6c17e3c7a2
Meet <149648810+Meet-official@users.noreply.github.com>
2024-04-04 03:03:04 +0530

    Update PhoneBook.cpp

--- PhoneBook.cpp
+++ PhoneBook.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
-// for string manipulation (like storing names).
+// For string manipulation (like storing names).
 #include <map>
-// for using a map data structure to store phonebook entries (key-value pairs).
+// For using a map data structure to store phonebook entries (key-value pairs).
 using namespace std;
 int main() {
     map<string,string> phonebook;
 /*  This line declares a map named phonebook. A map is a data structure that stores key-value pairs.
-    In this case, the key will be the name (string) and the value will be the phone number (string) associated with that name. */
+    In this case, the key will be the name (1st string) and the value will be the phone number (2nd string) associated with that name. */
     cout<<"Welcome to phonebook"<<endl;
     
     while(true){
@@ -27,8 +27,8 @@ int main() {
                 cin>>name;
                 cout<<"Enter Number"<<endl;
                 cin>>number;
-                phonebook[name]=number;
-// line adds a new entry to the phonebook map. The name becomes the key, and the phone number becomes the value associated with that key.
+                phonebook[name]=number;    // Basically links the entered name with entered phone number.
+                 // Line adds a new entry to the phonebook map. The name becomes the key, and the phone number becomes the value associated with that key.
                 cout<<"Contact added successfully"<<endl;
                 break;
             }
@@ -37,8 +37,11 @@ int main() {
                 string name;
                 cout<<"Enter Name"<<endl;
                 cin>>name;
-                if(phonebook.find(name) != phonebook.end())
-// jab ham find kar rhe hai aur jo store hai vo same na ho tab
+                if(phonebook.find(name) != phonebook.end()) // Jab ham find kar rhe hai aur jo store hai vo same na ho tab.
+                  /* .find(name) check karega ki entered name phonebook map me h ya nahi agar nahi hoga to wo check karte karte .end()/null pe pohoch jayega,
+                  and fir phonebook.end() != phonebook.end() hoga and ye condition false h to else me jayega and kahega contact not found. */
+                  /* Jab .find(name) ko entered value mil jaaye to
+                  example: mit != phonebook.end() hoga and ye codition true h to contact found print hoga. */
                 { cout<<"contact found\n"<<name<<"\n"<<phonebook[name]<<endl; }
                 else
                 { cout<<"does not exist"<<endl; }
@@ -48,7 +51,7 @@ int main() {
             {
                 cout<<"contact"<<endl;
                 for(const auto& entry:phonebook)
-// entry predefine, entry.first is key and entry.second is value par point kare
+                // Entry predefine, entry.first is key and entry.second is value par point kare.
                 { cout<<"Name: "<<entry.first<<"\nNumber: "<<entry.second<<endl; }
             }
             case 4:
