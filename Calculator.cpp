40796ad5a11176b4617cd00bb018ad5a01ae0f9f
Meet <149648810+Meet-official@users.noreply.github.com>
2024-04-04 00:39:38 +0530

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
-int power(int num1,int num2){
-    int n = 1;
+int power(int num1,int num2){   // num2 power h num1 ki.
+    int n = 1;    // n ek temp. var. h jisme num1 multiply hoke store hoga
     for (int i = 1; i <= num2; i++)
         n= n*num1;
     return n;}
 int main() {
-    char cal;
+    char opr;
     float num1,num2;
     cout <<"enter 1st numbers:"<< endl;
     cin >> num1;
-    cout <<"enter an operator (+, -, *, /,^):";
-    cin >> cal;
+    cout <<"enter an operator (+, -, *, /,^):";   // opr me jo operation perform karna h uska operator store hoga.
+    cin >> opr;
     cout <<"enter 2nd numbers:"<< endl;
     cin >> num2;
-    switch(cal){
+    switch(opr){
         case'+':
             cout<<num1<<"+"<<num2<<"="<<num1+num2;
             break;
