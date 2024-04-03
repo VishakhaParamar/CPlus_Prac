c82f04da84d737f3aee80d835a53c53571862b70
Meet <149648810+Meet-official@users.noreply.github.com>
2024-04-04 02:38:11 +0530

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 int main() {
     srand(time(NULL));
 //This line seeds the random number generator using the current time. This ensures different random numbers are generated on each program run.
-    int guess=rand()%100;
+    int guess=rand()%100; // 100 likhne pe 1 t0 100 me se random no. generate karega, agar 50 likho to 1 to 50.
 // This generates a random integer between 0 (inclusive) and 99 (exclusive) and stores it in the variable guess. This is the number the user needs to guess.
     int guessed,n=0;
     cout<<"enter the number b/w 1 to 100"<<endl;
