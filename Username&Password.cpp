7a78d713d3a5994114a8c2168799f06c6e78819b
Meet <149648810+Meet-official@users.noreply.github.com>
2024-04-04 01:25:55 +0530

    Update Username&Password.cpp

--- Username&Password.cpp
+++ Username&Password.cpp
@@ -4,25 +4,27 @@
 using namespace std;
 class login{
     private:
-    map<string,string>users;
+    map<string,string>users; // users map ka var. h jisme 1st string username ke liye h jo link h 2nd string password se.
     public:
-    bool registeruser(string username,string password){
-        if(users.find(username)==users.end()){
-            users[username]=password;
+    bool registeruser(string username,string password){ // registeruser is a func. which register a new user.
+        if(users.find(username)==users.end()){  //  It checks if the username already exists in the users map.
+            users[username]=password; // If doesn't exists, it adds the username-password pair to the map and return true.
             return true;
         }
-            return false;
+            return false; // If username already exists, it returns false.
     }
-    bool authentication(string username,string password){
+    bool authentication(string username,string password){ // method that checks username exists and password matches with that username.
         if(users.find(username)!=users.end()&&users[username]==password){
-            return true;
+          // 1st condition ye check karega ki username users map me h ya nahi.
+          // 2nd condition ye check karega ki username ke sath jo password h wo match karta h ki nahi.
+            return true;  // dono condition true hogi to true return hoga.
         }
            return false;
     }
 };
 int main(){
     login id;
-    id.registeruser("cat","cat888");
+    id.registeruser("cat","cat888");  // user register kiya h jisme cat username h and cat888 password h.
     string username,password;
     cout<<"enter username"<<endl;
     cin>>username;
