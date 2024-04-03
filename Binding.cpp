de00eb950b8369b05fdca5049b2d6bda9775ec95
Meet <149648810+Meet-official@users.noreply.github.com>
2024-04-04 01:55:01 +0530

    Update Binding.cpp

--- Binding.cpp
+++ Binding.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 class b{
     public:
-    virtual void show(){
+    virtual void show(){  // It is the base class with a virtual function show()
         cout<<"parent\n";
     }
 };
 class d:public b{
     public:
-    void show(){
+    void show(){  // It is derived from class b and overrides the show() function from the base class.
         cout<<"child\n";
     }
 }; 
 int main() {
-    b * bptr;
-    d obj;
-    bptr=&obj;
-    bptr->show();
+    b * bptr; // It declares a pointer bptr that can point to objects of class b or its derived classes.
+    d obj;  // Object with name obj of class d is created.
+    bptr=&obj;  // bptr stores the address of the obj object.
+    bptr->show(); // Calls the show() func. from class d because it overrides the virtual function of class b.
     return 0;
 }
