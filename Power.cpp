fa83f169ad41aaf25d6ba4df7151f50a760ed774
Vishakha Parmar <149483670+VishakhaMP@users.noreply.github.com>
2024-04-17 21:32:24 +0530

    Update Power.cpp

--- Power.cpp
+++ Power.cpp
@@ -20,3 +20,32 @@ int main() {
 }
 
 //2nd type
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main () {
+int b, p;
+cout << "Enter The Base: " << endl;
+cin >> b;
+cout << "Enter The Power: " << endl;
+cin >> p;
+cout << "Answer:" << pow(b,p) << endl;
+return 0;
+}
+
+//3rd type
+#include <iostream>
+using namespace std;
+int main () {
+int b, p, temp= 1, r;
+cout << "Enter The Base" << endl;
+cin >> b;
+cout << "Enter The Power" << endl;
+cin >> p;
+for (int i = 1; i <= p; i++) {
+r = b * temp;
+temp = r;
+}
+cout << "Answer: " <<r<< endl;
+return 0;
+}
